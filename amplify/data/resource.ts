9eb2e1092657b41ca89119f699ca5cc7afd96ee6
Penzi_ <101235314+PaoloAJ@users.noreply.github.com>
2025-11-28 17:11:01 -0500

    Finished, styling remains

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -16,7 +16,7 @@ const schema = a.schema({
       school: a.string().required(),
       pfp_key: a.string(),
       buddy_id: a.id(),
-      requests: a.id().array(),
+      request: a.id().array(),
       sent: a.id().array(),
     })
     .authorization((allow) => [allow.authenticated()]),
@@ -50,8 +50,6 @@ const schema = a.schema({
       time: a.datetime(),
     })
     .authorization((allow) => [allow.authenticated()]),
-
-
 });
 
 export type Schema = ClientSchema<typeof schema>;
